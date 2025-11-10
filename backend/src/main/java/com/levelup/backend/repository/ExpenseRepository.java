aa8fdff6a65cb012d7bb95203b19f9c02c4dff13
JasonLawson <jasonlawson301@gmail.com>
2025-11-10 17:07:18 -0500

    add findByUserId method

--- backend/src/main/java/com/levelup/backend/repository/ExpenseRepository.java
+++ backend/src/main/java/com/levelup/backend/repository/ExpenseRepository.java
@@ -4,6 +4,9 @@ import com.levelup.backend.model.Expense;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExpenseRepository  extends JpaRepository<Expense, Long> {
 
+    List<Expense> findByUserId(Long userId);
 }
