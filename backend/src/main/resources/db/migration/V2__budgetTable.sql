53ddb00592e945556488ce54024ccbec2712ab34
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2025-11-18 21:09:48 -0500

    Budget CRUD

--- backend/src/main/resources/db/migration/V2__budgetTable.sql
+++ backend/src/main/resources/db/migration/V2__budgetTable.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS budget;
 CREATE TABLE budget (
-                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                         category VARCHAR(100) NOT NULL,
-                         monthly_limit DOUBLE NOT NULL,
-                         start_date DATE,
-                         end_date DATE,
-                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        category VARCHAR(100) NOT NULL,
+                        monthly_limit DOUBLE NOT NULL,
+                        start_date DATE,
+                        end_date DATE,
+                        user_id BIGINT,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
