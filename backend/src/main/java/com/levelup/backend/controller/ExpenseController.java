b24cb77e901ec544fcc470442904a80a3e3b1d70
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2025-11-20 17:43:08 -0500

    request mapping /expenses to /api/expenses

--- backend/src/main/java/com/levelup/backend/controller/ExpenseController.java
+++ backend/src/main/java/com/levelup/backend/controller/ExpenseController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/expenses")
+@RequestMapping("/api/expenses")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ExpenseController {
 
